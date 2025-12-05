9399d2a640680cd445fcd31a474fdfb3d086e2e0
错数螺线 <3253615697@qq.com>
2025-12-05 08:49:21 +0800

    修复了在启用灾厄的时候主页预览会崩溃的问题

--- CoolerItemVisualEffectMod.cs
+++ CoolerItemVisualEffectMod.cs
@@ -5,8 +5,11 @@ global using Terraria.ID;
 global using Terraria.ModLoader;
 using LogSpiralLibrary;
 using Microsoft.Xna.Framework.Graphics;
+using MonoMod.Cil;
 using NetSimplified;
+using System;
 using System.IO;
+using System.Reflection;
 
 namespace CoolerItemVisualEffect;
 
@@ -24,8 +27,35 @@ public partial class CoolerItemVisualEffectMod : Mod
     #region NetSimplified
 
     public override void HandlePacket(BinaryReader reader, int whoAmI) => NetModule.ReceiveModule(reader, whoAmI);
-    public override void Load() => AddContent<NetModuleLoader>();
+    public override void Load()
+    {
+        AddContent<NetModuleLoader>();
 
+        if (ModLoader.TryGetMod("CalamityMod", out var calamity) && calamity.TryFind<GlobalProjectile>("CalamityGlobalProjectile", out var gbProj))
+        {
+            MonoModHooks.Modify(gbProj.GetType().GetMethod("PreDraw"), il => 
+            {
+                var cursor = new ILCursor(il);
+                var startLabel = cursor.MarkLabel();
+                cursor.Index = 0;
+                cursor.EmitLdsfld(typeof(Main).GetField(nameof(Main.gameMenu), BindingFlags.Static | BindingFlags.Public));
+                cursor.EmitBrfalse(startLabel);
+                cursor.EmitLdcI4(1);
+                cursor.EmitRet();
+            });
+
+            MonoModHooks.Modify(gbProj.GetType().GetMethod("GetAlpha"), il =>
+            {
+                var cursor = new ILCursor(il);
+                var startLabel = cursor.MarkLabel();
+                cursor.Index = 0;
+                cursor.EmitLdsfld(typeof(Main).GetField(nameof(Main.gameMenu), BindingFlags.Static | BindingFlags.Public));
+                cursor.EmitBrfalse(startLabel);
+                cursor.EmitLdnull();
+                cursor.EmitRet();
+            });
+        }
+    }
     #endregion
 
 }
